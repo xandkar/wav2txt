c8ec6f34ce5e988cf1e64b0f095e0a95ba72bd82
Siraaj Khandkar <siraaj@khandkar.net>
2025-05-07 18:13:48 -0400

    Download model to cache dir

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use std::path::PathBuf;
+use std::{fs, io, path::PathBuf};
 
+use anyhow::bail;
 use clap::Parser;
 use tracing::level_filters::LevelFilter;
+
+macro_rules! crate_name {
+    () => {
+        env!("CARGO_PKG_NAME")
+    };
+}
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -9,7 +16,7 @@ struct Cli {
     log_level: LevelFilter,
 
     #[clap(short, long)]
-    model_file: PathBuf,
+    model_file: Option<PathBuf>,
 
     /// Input audio file.
     #[clap(short, long)]
@@ -26,13 +33,17 @@ struct Cli {
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     tracing_init(cli.log_level)?;
-    let span = tracing::debug_span!(env!("CARGO_PKG_NAME"));
+    let span = tracing::debug_span!(crate_name!());
     let _span_guard = span.enter();
     tracing::info!(?cli, "Starting.");
+    let model_file = match cli.model_file {
+        Some(path) => path,
+        None => model_download()?,
+    };
     wav2txt::convert(
         &cli.input_file,
         cli.output_file.as_deref(),
-        &cli.model_file,
+        &model_file,
         cli.normalize,
     )?;
     Ok(())
@@ -66,4 +77,39 @@ fn tracing_init(level: LevelFilter) -> anyhow::Result<()> {
         tracing_subscriber::registry().with(layer_stderr),
     )?;
     Ok(())
+}
+
+fn model_download() -> anyhow::Result<PathBuf> {
+    let cache_dir = match dirs::cache_dir() {
+        Some(path) => path,
+        None => {
+            tracing::warn!(
+                "No system cache directory found. Falling back on temporary."
+            );
+            tempfile::tempdir()?.into_path()
+        }
+    }
+    .join(crate_name!());
+    tracing::debug!(?cache_dir, "Determined cache directory.");
+    let model_name = "base.en";
+    let model_file_name = format!("ggml-{model_name}.bin");
+    let model_file_path = cache_dir.join("models").join(&model_file_name);
+    if !model_file_path.try_exists()? {
+        if let Some(parent) = model_file_path.parent() {
+            fs::create_dir_all(parent)?;
+        }
+        let model_url = format!("https://huggingface.co/ggerganov/whisper.cpp/resolve/main/{model_file_name}");
+        tracing::info!(src = ?model_url, dst = ?model_file_path, "Model file not found. Downloading.");
+        let resp = reqwest::blocking::get(model_url)?;
+        let resp_status = resp.status();
+        if !resp_status.is_success() {
+            bail!(
+                "Failed to download model file. Response status: {resp_status}"
+            );
+        }
+        let mut dst = io::BufWriter::new(fs::File::create(&model_file_path)?);
+        let mut src = resp;
+        io::copy(&mut src, &mut dst)?;
+    }
+    Ok(model_file_path)
 }
